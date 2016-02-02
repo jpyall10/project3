904363b8ddf0391ab950532f3907c0041b396cbb
Jonathan Porter <jonathanporter@JonathasMacBook.hsd1.mi.comcast.net>
2016-02-02 18:58:46 -0500

    Content Descriptions added RTL localization enabled Widget has been added but uses fake simple list data

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/WidgetService.java
@@ -0,0 +1,15 @@
+package barqsoft.footballscores;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+/**
+ * Created by jonathanporter on 1/31/16.
+ */
+public class WidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        //return remote View Factor here
+        return new WidgetDataProvider(this, intent);
+    }
+}
---
+++ alexandria/app/src/main/java/google/zxing/integration/android/IntentResult.java
@@ -0,0 +1,7 @@
+package google.zxing.integration.android;
+
+/**
+ * Created by jonathanporter on 1/21/16.
+ */
+public class IntentResult {
+}
